2cd653cb32e5938551ee8c3bddf0cda52e3b8f4d
prathmesh-narkar <prathmesh.narkar199@gmail.com>
2024-05-12 18:13:57 +0530

    Initial commit

---
+++ src/main/java/com/edureka/demo/AprilWeekenMorningDemoApplication.java
@@ -0,0 +1,13 @@
+package com.edureka.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprilWeekenMorningDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprilWeekenMorningDemoApplication.class, args);
+	}
+
+}
